4094e37a5947a1b8a4eae935cb39d90204202f81
Andy George <andrew.m.george@gmail.com>
2022-08-16 11:34:47 -0500

    add systemd support

--- injection.py
+++ injection.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
-import os
-import sys
+from os import system,chmod,makedirs
+from os.path import exists
+from sys import exit
 from json import load
 from base64 import b64decode
 from urllib.parse import unquote
 from gzip import decompress
+from argparse import ArgumentParser
+
+SYSTEMD_UNIT_PATH = '/etc/systemd/system'
 
 def main():
-  ign_filename = sys.argv[1]
+  parser = ArgumentParser(description='Process an Ignition file on an already-running system.')
+  parser.add_argument('path', help='Path to a valid Ignition file')
+  args = parser.parse_args()
+  ign_filename = args.path
+
+  if not exists(ign_filename):
+    print(f'{ign_filename} does not exist, please enter a valid Ignition file.')
+    exit()
   with open(ign_filename, 'r') as fh: ign_json = load(fh)
 
   ign_directories = ign_json.get('storage', {}).get('directories', {})
@@ -17,8 +28,8 @@ def main():
   for dir in ign_directories:
     path = dir['path']
     mode = int(dir['mode'])
-    if not os.path.exists(path): os.makedirs(path, mode)
-    os.chmod(path, mode)
+    if not exists(path): makedirs(path, mode)
+    chmod(path, mode)
 
   for file in ign_files:
     path = file['path']
@@ -27,18 +38,30 @@ def main():
     content_compression = dict_contents.get('compression', '')
     content_source = dict_contents.get('source', '')
 
-    if content_compression == "gzip":
+    if content_compression == 'gzip':
       base64encoded_string = content_source.replace('data:;base64,', '')
       gzipped_string = b64decode(base64encoded_string)
       decompressed_bytestring = decompress(gzipped_string)
-      the_content = str(decompressed_bytestring)[2:-1]
+      raw_string = str(decompressed_bytestring)[2:-1]
+      the_content = raw_string.replace('\\n', '\n').replace('\\\\', '\\')
 
-      # \n !!!!
       with open(path, 'w') as fh: fh.write(the_content)
     else:
       quoted_string = content_source.replace('data:,', '')
       the_content = unquote(quoted_string)
       with open(path, 'w') as fh: fh.write(the_content)
+
+  for unit in ign_units:
+    name = unit['name']
+    path = f'{SYSTEMD_UNIT_PATH}/{name}'
+    enabled = bool(unit['enabled'])
+    content = unit['contents']
+
+    with open(path, 'w') as fh: fh.write(content)
+    if enabled:
+      system(f'systemctl enable {name}')
+      system('systemctl daemon-reload')
+      system(f'systemctl start {name}')
 
 if __name__ == "__main__":
   main()
